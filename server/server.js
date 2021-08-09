6e19c8cf560d2a33461c5e65187daf82729212b7
kineretasiyahan <kineret9688@gmail.com>
2021-08-09 20:48:39 +0300

    change script start

--- server/server.js
+++ server/server.js
@@ -2,24 +2,31 @@ const dotenv = require('dotenv').config();
 const PORT = process.env.PORT | 2300;
 const express = require('express');
 const app = express();
-const mongoClient = require('mongodb').MongoClient;
-const MONGOURL = process.env.MONGOURL|"mongodb://localhost:27017/dbblog";
+// const mongoClient = require('mongodb').MongoClient;
+// const MONGOURL = "mongodb+srv://kineret:Aa123456!@cluster0.udluj.mongodb.net/myFirstDataBase?retryWrites=true&w=majority";
+const path = require('path')
 const moongoseDb =require('./Db/index');
 const personRouter=require('./Routes/PersonRouter')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 moongoseDb.on("error",()=>{console.log("moongose error")})
 
-mongoClient.connect(MONGOURL, (error, connection) => {
-    if (error) console.log(error);
-    console.log("connect")})
+// mongoClient.connect(MONGOURL, (error, connection) => {
+//     if (error) console.log(error);
+//     console.log("connect")})
 
 app.listen(PORT, (err) => {
     if (err) return console.log(err);
     console.log("succses");
 });
-app.get('/', (req, res) => {
-    res.send("sucsses");
-})
+// app.get('/', (req, res) => {
+//     res.send("sucsses");
+// })
+app.use('api/person',personRouter)
 
-app.use('/person',personRouter)
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../client/build')))
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../client/build','index.html'))
+    })
+}
